472f1cd23eb2affc7c6f1f0032be5c757ff00e9c
Manish Chaudhary <manishchaudharyog@gmail.com>
2025-12-08 19:49:22 +0530

    pricing create

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import './index.css';
 import { Route, Routes } from "react-router-dom";
 import Home from "./page/homePage";
+import Pricing from './page/pricing';
 function App() {
 
 
@@ -9,6 +10,7 @@ function App() {
  <div>
   <Routes>
     <Route path="/" element={<Home/>} />
+    <Route path="/pricing" element={<Pricing/>} />
   </Routes>
  </div>
   )
---
+++ src/page/wall-of-love.tsx
@@ -0,0 +1,12 @@
+<div className="form-switch focus-within:none">
+  <input
+    type="checkbox"
+    name="pricing-toggle"
+    id="pricing-toggle"
+    className="sr-only"
+    checked
+  />
+  <label className="bg-gray-600" form="pricing-toggle">
+    <span className="bg-white dark:bg-gray-200" aria-hidden="true"></span>
+  </label>
+</div>
